2bfb5c9349b4a8d111b7e5d7f439ee388df849f9
Rohit <Rohit@Rohits.local>
2018-09-11 20:11:00 +1200

    Intial Commit to understand node projects

---
+++ server.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const mustacheExpress = require('mustache-express');
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+const routes = require('./routes/routes');
+
+mongoose.Promise = global.Promise;
+
+mongoose.connect('mongodb://localhost:27017/mongoose_express_todos', {useNewUrlParser: true }).then(function(){
+  console.log('Database connected');
+});
+
+const app = express();
+app.use(bodyParser.urlencoded({ extended: true }));
+
+const mustacheExpressInstance = mustacheExpress();
+mustacheExpressInstance.cache = null;
+app.engine('mustache', mustacheExpressInstance);
+app.set('view engine', 'mustache');
+app.set('views', __dirname + '/views');
+
+app.use('/', routes);
+
+
+app.listen(3000, function(){
+  console.log('listening on port 3000');
+});
